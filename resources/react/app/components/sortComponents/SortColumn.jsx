223bf7ac64d3d4ffaa015c43b72729c1100466fc
Kyle Widmann <Kyle.Widmann@gmail.com>
2017-10-06 11:43:21 -0500

    Implemented drag and drop for sorting.  Very clunky, need to improve

--- resources/react/app/components/sortComponents/SortColumn.jsx
+++ resources/react/app/components/sortComponents/SortColumn.jsx
@@ -2,110 +2,77 @@ import React, {Component} from 'react';
 import SortingStickyNote from 'Components/SortingStickyNote';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 
-const columnStyle = {
-  flexGrow: 1,
-  flexBasis: 0,
-};
-
 const borderStyle = {
   borderTop: '1px solid #eee',
 };
 
-// a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+const grid = 2;
 
-  return result;
-};
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'white',
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? 'lightblue' : 'white',
+  width: 250,
+  flexGrow: 1,
+  flexBasis: 0,
+});
 
 export default class SortColumn extends Component{
 
   constructor(props){
     super(props);
-    this.state = {
-      items: [
-        {
-          text: 'Example',
-          dueDate: '9/1/2017',
-          category: 'home',
-        },
-        {
-          text: 'Longer Example - lorem ipsum',
-          dueDate: '9/1/2017',
-          category: 'school',
-        },
-        {
-          text: 'Longest Example - lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
-          dueDate: '9/1/2017',
-          category: 'school',
-        },
-        {
-          text: 'Longer Example - lorem ipsum',
-          dueDate: '9/1/2017',
-          category: 'work',
-        },
-        {
-          text: 'Longest Example - lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
-          dueDate: '9/1/2017',
-          category: 'home',
-        },
-        {
-          text: 'Example',
-          dueDate: '9/1/2017',
-          category: 'work',
-        },
-      ]
-    }
-
   };
-
-  onDragEnd = (result) => {
-    // dropped outside the list
-    if (!result.destination) {
-      return;
-    }
-
-    const items = reorder(
-      this.state.items,
-      result.source.index,
-      result.destination.index
-    );
-
-    this.setState({
-      items,
-    });
-  };
-
-  shuffleArray = (arr) => arr.sort(() => (Math.random() - 0.5));
 
   render(){
 
-    this.shuffleArray(this.state.items);
-
     return(
-      <Droppable droppableId="droppable">
+      <Droppable droppableId={this.props.id}>
         {(provided, snapshot) => (
-          <div ref={provided.innerRef}>
-            <div style={columnStyle}>
-              <h1 style={{textAlign: 'center'}}>{this.props.title}</h1>
-              <div style={Object.assign({}, borderStyle, this.props.style)}>
-                {this.state.items.map((item, index) => (
-                  <Draggable key={index} draggableId={index}>
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            <h1 style={{textAlign: 'center'}}>{this.props.title}</h1>
+            <div style={Object.assign({}, borderStyle, this.props.style)}>
+              {
+                this.props.items.map((item, index) => (
+                  <Draggable
+                    key={this.props.id + '-' + index}
+                    draggableId={this.props.id + '-' + index}
+                  >
                     {(provided, snapshot) => (
                       <div>
-                        <SortingStickyNote
-                          data={item}
+                        <div
                           ref={provided.innerRef}
+                          style={getItemStyle(
+                            provided.draggableStyle,
+                            snapshot.isDragging
+                          )}
                           {...provided.dragHandleProps}
-                        />
+                        >
+                          <SortingStickyNote
+                            data={item}
+                            ref={provided.innerRef}
+                            {...provided.dragHandleProps}
+                          />
+                        </div>
                         {provided.placeholder}
                       </div>
                     )}
                   </Draggable>
                 ))}
-              </div>
+              {provided.placeholder}
             </div>
           </div>
         )}
